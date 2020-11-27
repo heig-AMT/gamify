ad1d125db698dd2cd8bff8d54f1416622bf33999
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-11-27 11:35:32 +0100

    Implement login

--- gamify-impl/src/main/java/ch/heigvd/gamify/api/endpoints/AuthenticationApiController.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/api/endpoints/AuthenticationApiController.java
@@ -1,24 +1,33 @@
 package ch.heigvd.gamify.api.endpoints;
 
+import ch.heigvd.gamify.api.LoginApi;
 import ch.heigvd.gamify.api.RegisterApi;
 import ch.heigvd.gamify.api.model.AuthenticationSuccess;
 import ch.heigvd.gamify.api.model.Credentials;
 import ch.heigvd.gamify.entities.RegisteredAppEntity;
 import ch.heigvd.gamify.repositories.RegisteredAppRepository;
+import java.util.Optional;
 import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.NativeWebRequest;
 
 @Controller
-public class RegistrationApiController implements RegisterApi {
+public class AuthenticationApiController
+    implements RegisterApi, LoginApi {
 
   private final RegisteredAppRepository repository;
 
-  public RegistrationApiController(RegisteredAppRepository repository) {
+  public AuthenticationApiController(RegisteredAppRepository repository) {
     this.repository = repository;
+  }
+
+  @Override
+  public Optional<NativeWebRequest> getRequest() {
+    return Optional.empty();
   }
 
   @Transactional
@@ -37,5 +46,13 @@ public class RegistrationApiController implements RegisterApi {
     repository.save(entity);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(new AuthenticationSuccess().token(token));
+  }
+
+  @Override
+  public ResponseEntity<AuthenticationSuccess> login(@Valid Credentials credentials) {
+    return repository.findById(credentials.getAppId())
+        .filter((app) -> app.getPassword().equals(credentials.getPassword()))
+        .map((app) -> ResponseEntity.status(HttpStatus.OK).body(new AuthenticationSuccess().token(app.getToken())))
+        .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
   }
 }
