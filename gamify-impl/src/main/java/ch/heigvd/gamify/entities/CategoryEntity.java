9e08ce9ef0a1f39ad557f8dd241c7a817a8c4552
David <duprazdavid@gmail.com>
2020-11-27 15:31:20 +0100

    first endpoint, the other files

--- gamify-impl/src/main/java/ch/heigvd/gamify/entities/CategoryEntity.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/entities/CategoryEntity.java
@@ -1,2 +1,21 @@
-package ch.heigvd.gamify.entities;public class Category {
+package ch.heigvd.gamify.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryEntity implements Serializable
+{
+    @Id
+    private String name;
 }
--- gamify-impl/src/main/java/ch/heigvd/gamify/repositories/CategoryRepository.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/repositories/CategoryRepository.java
@@ -1,2 +1,8 @@
-package ch.heigvd.gamify.repositories;public interface CategoryRepository {
+package ch.heigvd.gamify.repositories;
+
+import ch.heigvd.gamify.entities.CategoryEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<CategoryEntity, String >
+{
 }
