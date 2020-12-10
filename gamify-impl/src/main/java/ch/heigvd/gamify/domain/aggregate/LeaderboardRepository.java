26cdc7ad3c9e1d3dcceb5476921ad6e79bc712c1
Alexandre Piveteau <alexandre@piveteau.email>
2020-12-11 00:21:28 +0100

    Fix missing category response code

--- gamify-impl/src/main/java/ch/heigvd/gamify/domain/aggregate/LeaderboardRepository.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/domain/aggregate/LeaderboardRepository.java
@@ -2,18 +2,17 @@ package ch.heigvd.gamify.domain.aggregate;
 
 import ch.heigvd.gamify.domain.rule.Rule;
 import ch.heigvd.gamify.domain.rule.RuleIdentifier;
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
-
-import java.util.List;
 
 public interface LeaderboardRepository extends Repository<Rule, RuleIdentifier> {
 
   @Query("SELECT SUM(r.points) AS total, e.user AS user "
-                  + "FROM Rule r INNER JOIN Event e ON r.eventType = e.type AND r.id.app = e.app "
-                  + "WHERE e.app = :app "
-                  + "AND r.category.idCategory.name = :category "
-                  + "GROUP BY e.user "
-                  + "ORDER BY total")
+      + "FROM Rule r INNER JOIN Event e ON r.eventType = e.type AND r.id.app = e.app.name "
+      + "WHERE e.app.name = :app "
+      + "AND r.category.idCategory.name = :category "
+      + "GROUP BY e.user "
+      + "ORDER BY total")
   List<LeaderboardEntry> findLeaderboardEntries(String app, String category);
 }
--- gamify-impl/src/main/java/ch/heigvd/gamify/ui/api/endpoints/CategoryAggregatesController.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/ui/api/endpoints/CategoryAggregatesController.java
@@ -4,10 +4,12 @@ import ch.heigvd.gamify.api.LeaderboardsApi;
 import ch.heigvd.gamify.api.model.Ranking;
 import ch.heigvd.gamify.domain.aggregate.LeaderboardRepository;
 import ch.heigvd.gamify.domain.app.App;
+import ch.heigvd.gamify.domain.category.CategoryRepository;
 import ch.heigvd.gamify.ui.api.filters.ApiKeyFilter;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,17 +23,26 @@ import java.util.stream.Collectors;
 public class CategoryAggregatesController implements LeaderboardsApi {
 
   @Autowired
+  private CategoryRepository categoryRepository;
+
+  @Autowired
   private LeaderboardRepository leaderboardRepository;
 
   @Autowired
   private ServletRequest request;
 
+  @Transactional
   @Override
   public ResponseEntity<List<Ranking>> getLeaderboard(@ApiParam(value = "name of the category",required=true) @PathVariable("name") String name,
                                                       @ApiParam(value = "") @Valid @RequestParam(value = "page", required = false) Integer page,
                                                       @ApiParam(value = "") @Valid @RequestParam(value = "size", required = false) Integer size) {
     //var pageable = PageRequest.of(page == null ? 0 : page, size == null ? Integer.MAX_VALUE : size);
     var app = (App) request.getAttribute(ApiKeyFilter.APP_KEY);
+    var category = categoryRepository.findByIdCategory_AppAndIdCategory_Name(app, name);
+
+    if (category.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
 
     var leaderboard = leaderboardRepository.findLeaderboardEntries(app.getName(), name)
             .stream()
