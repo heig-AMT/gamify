d9aa28fb38b1a938fc7bf2512072f03937747e8a
matt989253 <matthieu.burguburu@gmail.com>
2020-12-23 08:45:10 +0100

    Implement badge logic for aggregates

--- gamify-impl/src/main/java/ch/heigvd/gamify/domain/aggregate/RankingRepository.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/domain/aggregate/RankingRepository.java
@@ -9,12 +9,16 @@ import java.util.List;
 
 public interface RankingRepository extends Repository<Rule, RuleIdentifier> {
 
-  @Query("SELECT r.category.idCategory.name AS category, e.user AS user, SUM(r.points) AS total "
-      + "FROM Rule r INNER JOIN Event e ON r.eventType = e.type AND r.id.app = e.app.name "
+  @Query("SELECT r.category.idCategory.name AS category, e.user AS user, SUM(r.points) AS total, b.title AS badge "
+      + "FROM Rule r "
+      + "INNER JOIN Event e ON r.eventType = e.type AND r.id.app = e.app.name "
+      + "INNER JOIN Badge b ON b.category = r.category AND b.idBadge.app = r.id.app "
       + "WHERE e.app.name = :app "
       + "AND e.user = :userId "
       + "AND r.category.idCategory.name = :category "
-      + "GROUP BY r.category.idCategory.name, e.user "
-      + "ORDER BY total DESC")
+      + "GROUP BY r.category.idCategory.name, e.user, b.title, b.pointsUpper, b.pointsLower "
+      + "HAVING SUM(r.points) > b.pointsLower "
+      + "AND SUM(r.points) < b.pointsUpper "
+      + "ORDER BY total DESC ")
   List<RankingEntry> findRankingEntryForUserAndCategory(String app, String userId, String category);
 }
