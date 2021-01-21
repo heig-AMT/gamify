9953aa76378ecedfe260921698379dbd8cf277da
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-21 15:23:56 +0100

    Use GitHub Container Registry instead of GitHub Packages

--- publish-image-github.sh
+++ publish-image-github.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Tag the Docker image.
-docker tag gamify-openjdk docker.pkg.github.com/heig-amt/gamify/openjdk
+docker tag gamify-openjdk ghcr.io/heig-amt/gamify-openjdk
 
 # Log in to GitHub package registry.
-echo $TOKEN_GITHUB | docker login https://docker.pkg.github.com -u heig-AMT-bot --password-stdin
+echo $TOKEN_GITHUB | docker login ghcr.io -u heig-AMT-bot --password-stdin
 
 # Push the Docker image.
-docker push docker.pkg.github.com/heig-amt/gamify/openjdk
+docker push ghcr.io/heig-amt/gamify-openjdk
