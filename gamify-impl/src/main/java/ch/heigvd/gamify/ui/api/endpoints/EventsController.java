e0a854927b2d582065944969cd0f2e7ab19893c4
matt989253 <matthieu.burguburu@gmail.com>
2021-01-23 11:36:17 +0100

    Reformat

--- gamify-impl/src/main/java/ch/heigvd/gamify/ui/api/endpoints/EventsController.java
+++ gamify-impl/src/main/java/ch/heigvd/gamify/ui/api/endpoints/EventsController.java
@@ -61,23 +61,23 @@ public class EventsController implements EventsApi {
 
   private void addEventPointAward(String username, App app, Rule rule, int points) {
     var endUser = userRepository
-            .findByIdEndUser_UserIdAndIdEndUser_App(username, app)
-            .orElse(userRepository.save(
-                    EndUser.builder()
-                            .idEndUser(
-                                    EndUserIdentifier.builder()
-                                            .userId(username)
-                                            .app(app).build()
-                            )
-                            .build())
-            );
+        .findByIdEndUser_UserIdAndIdEndUser_App(username, app)
+        .orElse(userRepository.save(
+            EndUser.builder()
+                .idEndUser(
+                    EndUserIdentifier.builder()
+                        .userId(username)
+                        .app(app).build()
+                )
+                .build())
+        );
 
     userPointAwardRepository.save(
-      EndUserPointAward.builder()
-              .idxCategory(rule.getCategory())
-              .idxEndUser(endUser)
-          .points(points)
-          .build()
+        EndUserPointAward.builder()
+            .idxCategory(rule.getCategory())
+            .idxEndUser(endUser)
+            .points(points)
+            .build()
     );
   }
 }
